9992423165b1ec1acd4085c2065ef47add10f181
Adam Ralph <adam@adamralph.com>
2017-05-19 09:10:28 +0300

    prepare the scene for magic

---
+++ NetCoreApp20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NetCoreApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Abracadabra...");
+
+            //Bag<int> bag = new Bag<int>() { 1, 2, 3 };
+            //foreach (var i in bag)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            Console.WriteLine("Hey presto!");
+            Console.ReadKey();
+        }
+    }
+}
